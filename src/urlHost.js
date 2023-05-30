ffa4df0fb31fc00d41283893de5981b2bf8faf7c
wenchijchang <heyyhey@hotmail.com>
2023-05-30 18:02:37 -0400

    fixed: updated URL with new proxy

--- src/urlHost.js
+++ src/urlHost.js
@@ -1,4 +1,4 @@
 export const URL_HOST =
   process.env.NODE_ENV === "production"
-    ? "https://cors-everywhere-me.herokuapp.com/http://dccmusiclibrarybackend-env.eba-xevmbssv.us-east-2.elasticbeanstalk.com"
+    ? "https://worker-proxy1.heyyhey.workers.dev/?targetUrl=http://dccmusiclibrarybackend-env.eba-xevmbssv.us-east-2.elasticbeanstalk.com"
     : "http://127.0.0.1:8000";
