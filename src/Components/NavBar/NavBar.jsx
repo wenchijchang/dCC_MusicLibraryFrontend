a80bc192c66be1440c8b0accafa3452928756160
wenchijchang <heyyhey@hotmail.com>
2023-05-30 18:34:17 -0400

    feat: updated NavBar welcome message

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,22 +1,17 @@
-import './NavBar.css'
-import { Navbar, Container } from 'react-bootstrap';
-import {ReactComponent as Headphones} from './headphones.svg'
-
-
+import "./NavBar.css";
+import { Navbar, Container } from "react-bootstrap";
+import { ReactComponent as Headphones } from "./headphones.svg";
 
 const NavBar = (props) => {
-    return ( 
-        <Navbar bg="info" variant="info">
-        <Container className={"container-fluid"} >
-          <Navbar.Brand href="#home" style={{"color": "white"}} >
-            <Headphones height='3rem'/>{' '}
-          The Magnificent Music Library!
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
+  return (
+    <Navbar bg="info" variant="info">
+      <Container className={"container-fluid"}>
+        <Navbar.Brand href="#home" style={{ color: "white" }}>
+          <Headphones height="3rem" /> Welcome To The Magnificent Music Library!
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
 
-
-     );
-}
- 
 export default NavBar;
